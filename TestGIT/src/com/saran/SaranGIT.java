753cdd75710c6765459141eb9b0da7c5bdf64735
CogentInfotech <infotechcogent@gmail.com>
2017-06-01 15:57:26 -0400

    Update SaranGIT.java

--- TestGIT/src/com/saran/SaranGIT.java
+++ TestGIT/src/com/saran/SaranGIT.java
@@ -1,11 +1,8 @@
 package com.saran;
 
 public class SaranGIT {
-	
+
 	public static void main(String[] args) {
 		System.out.println("welcome to git");
 	}
-	//This is the change
-//	This is my second change
-
 }
